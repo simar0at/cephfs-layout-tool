31d22954f5190cf9663bf84932e21cc5abb52e0a
Omar Siam <simar@gmx.net>
2023-05-03 22:35:31 +0200

    Fix user/group is lost
    shutil.copy2 also does not work as expected.

--- cephfs_layout_tool/migrate_pools.py
+++ cephfs_layout_tool/migrate_pools.py
@@ -78,11 +78,12 @@ def mkdtemp_layout(layout: CephLayout, prefix: str) -> str:
 
 def relayout_file(filename, tmploc):
     logging.info("copying {} to temp location {}".format(filename, tmploc))
-    shutil.copy2(filename, tmploc)
-    logging.info("moving back on top of original")
-    # shutil.move(tmploc, filename) or any python file copy function does not work here (anymore).
-    # The data would end up on the same pool it came from.
+    # shutil.copy2(filename, tmploc) or any python file copy function does not work here (anymore).
+    # The data would end up on the same pool it came from and user and group are lost.
     # Is this a side effect of the sendfile zero copy trick?
+    subprocess.run(['cp', '-a', filename, tmploc])
+    logging.info("moving back on top of original")
+    # shutil.move(tmploc, filename)
     subprocess.run(['mv', tmploc, filename])
 
 def main():
